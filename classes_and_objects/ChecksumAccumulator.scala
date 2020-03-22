fa728feee83f1b327c86e4dabcb5b29c50aff27a
gabyrobles93 <gabyrobles93@gmail.com>
2020-03-22 19:49:07 -0300

    singleton object with main example

--- classes_and_objects/ChecksumAccumulator.scala
+++ classes_and_objects/ChecksumAccumulator.scala
@@ -4,32 +4,24 @@ class ChecksumAccumulator {
 
   private var sum = 0
 
-  def add(b: Byte) = sum += b
+  def add(b: Byte): Unit = { sum += b }
 
-  def checksum() : Int = ~(sum & 0xFF) + 1
+  def checksum(): Int = ~(sum & 0xFF) + 1
 }
 
-// Singleton object: the Class Companion Object
-// If it exists, you can't create a variable of type ChecksumAccumulator
-// Rather, the type named ChecksumAccumulator is defined by the singleton object's companion class
 object ChecksumAccumulator {
 
   private val cache = mutable.Map.empty[String, Int]
 
-  def calculate(s: String) : Int = {
-    if(cache.contains(s))
+  def calculate(s: String): Int =
+    if (cache.contains(s))
       cache(s)
     else {
       val acc = new ChecksumAccumulator
-      s.foreach(c => acc.add(c.toByte))
+      for (c <- s)
+        acc.add(c.toByte)
       val cs = acc.checksum()
       cache += (s -> cs)
       cs
     }
-  }
 }
-
-// Invocation a method of the Singleton Object
-val cs = ChecksumAccumulator.calculate("Se le calculará el checksum a este String xD")
-
-println("Checksum: " + cs)
